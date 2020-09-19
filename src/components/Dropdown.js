1d60ebcb93bb4a30c7f319e82c7cecbc8cb2c027
aaronclare3 <aaronclare88@gmail.com>
2020-09-19 15:07:44 -0700

    working on Translate widget

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,7 +1,32 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
-const Dropdown = ({ options, selected, onSelectedChange }) => {
+const Dropdown = ({ options, selected, onSelectedChange, label }) => {
     const [open, setOpen] = useState(false);
+    const ref = useRef();
+
+    useEffect(() => {
+        // IF THE CURRENT ELEMENT THAT IS CLICKED ON IS 
+        // INSIDE THE ITEM ELEMENT OF DROPDOWN 
+        // COMPONENT, THEN JUST RETURN (BECAUSE WE ALREADY
+        // HAVE AN ONCLICK FOR THAT). BUT IF CLICK IS 
+        // ANYWHERE ELSE, THEN CLOSE THE DROPDOWN
+
+        const onBodyClick = (e) => {
+            if (ref.current.contains(e.target)) {
+                return;
+            }
+            setOpen(false);
+        }
+
+        document.body.addEventListener('click', onBodyClick);
+
+
+        return () => {
+            document.body.removeEventListener('click', onBodyClick);
+        };
+
+    },[]);
+ 
 
     const renderedOptions = options.map((option) => {
         if(option.value === selected.value) {
@@ -17,12 +42,13 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
                 {option.label}
             </div>
         )
-    })
+    });
+
     return (
         <div className="ui form">
             <div className="field">
-                <label className="label">Select a Color</label>
-                <div onClick={() => setOpen(!open)} className={`ui selection dropdown ${open ? 'visible active' : ''}`}>
+                <label className="label">Select a {label}</label>
+                <div ref={ref} onClick={() => setOpen(!open)} className={`ui selection dropdown ${open ? 'visible active' : ''}`}>
                     <i className="dropdown icon"></i>
                     <div className="text">{selected.label}</div>
                     <div className={`menu ${open? 'visible transition': ''}`}>
